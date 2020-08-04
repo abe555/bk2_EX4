e7a230bd514b626f12742e56037856702ccbad7b
abe555 <riho.has3v@gmail.com>
2020-08-04 19:22:59 +0900

    Fix spellmiss in user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,10 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image, destroy: :false
 
-  has_many :active_relationship, class_name: "Relationship", foregin_key: :following_id
+  has_many :active_relationship, class_name: "Relationship", foreign_key: :following_id
   has_many :followings, through: :active_relationships, source: :follower
-  has_many :passive_relationship, class_name: "Relationship", foregin_key: :follower_id
-  has_many :followers, through: :passive_relationships, sorce: :following
+  has_many :passive_relationship, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
 
   def followed_by?(user)
   	passive_relationships.find_by(following_id: user.id).present?
