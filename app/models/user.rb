3ce043ee937788149f11377b6ab1418fc47ddf5e
abe555 <riho.has3v@gmail.com>
2020-08-04 21:41:06 +0900

    Add follow_function UserIndexPages

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image, destroy: :false
 
-  has_many :active_relationship, class_name: "Relationship", foreign_key: :following_id
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
   has_many :followings, through: :active_relationships, source: :follower
-  has_many :passive_relationship, class_name: "Relationship", foreign_key: :follower_id
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
   has_many :followers, through: :passive_relationships, source: :following
 
   def followed_by?(user)
